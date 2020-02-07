c42afe327528943dada514b30944c08ff3e4e990
Maksim Kuzmin <maxim.seid@gmail.com>
2020-02-07 13:20:07 +0300

    Added settings link under plugin actions

--- bible-online-popup.php
+++ bible-online-popup.php
@@ -13,7 +13,7 @@ require_once "tiny-mce.php";
 
 function bible_popup_func($atts, $content = "") {
     $options_defaults = [
-      "default_trans" => "rst66"
+        "default_trans" => "rst66"
     ];
     $options = wp_parse_args(get_option("bop_general"), $options_defaults);
 
@@ -33,7 +33,7 @@ function bible_popup_func($atts, $content = "") {
     return $html;
 }
 
-function add_scripts() {
+function bop_add_scripts() {
     wp_enqueue_script('bop-popup-js', plugins_url('js/bible-online-popup.js', __FILE__), ['jquery'], false, true);
     wp_enqueue_style('bop-css', plugins_url('css/bible-online-popup.css', __FILE__));
     wp_enqueue_style('bop-spinner', plugins_url('css/spinner.css', __FILE__));
@@ -43,7 +43,20 @@ function bop_load_plugin_textdomain() {
     load_plugin_textdomain('BOP', FALSE, basename(dirname(__FILE__)) . '/languages/');
 }
 
+function bop_plugin_actions($actions, $plugin_file, $plugin_data, $context) {
+    array_unshift($actions,
+        sprintf('<a href="%s" aria-label="%s">%s</a>',
+            menu_page_url('bop-settings', false),
+            esc_attr__('Bible Online Popup Settings', 'BOP'),
+            esc_html__("Settings", 'default')
+        )
+    );
+    return $actions;
+}
+
+add_filter("plugin_action_links_" . plugin_basename(__FILE__), "bop_plugin_actions", 10, 4);
+
 add_action('plugins_loaded', 'bop_load_plugin_textdomain');
-add_action('wp_enqueue_scripts', 'add_scripts');
+add_action('wp_enqueue_scripts', 'bop_add_scripts');
 
 add_shortcode('bible_popup', 'bible_popup_func');
